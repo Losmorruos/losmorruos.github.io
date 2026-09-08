c003c4173bd41c6a2dc976e27b8e913c2360ed00
Losmorruos <clubdeportivolosmorruos@gmail.com>
2026-09-09 00:11:57 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
 
 // v18: Firebase global + migración automática de usuarios antiguos.
-const CACHE = "morruos-v26-push-folder";
+const CACHE = "morruos-v28-root-onesignal";
 const ASSETS = [
   "./index.html",
   "./manifest.json",
